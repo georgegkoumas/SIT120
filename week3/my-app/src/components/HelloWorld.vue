5c5917bed33fd0b48bde8721f6e38226ee526147
George Gkoumas <agoriani@gmail.com>
2023-07-31 18:32:19 +1000

    Update HelloWorld.vue

--- week3/my-app/src/components/HelloWorld.vue
+++ week3/my-app/src/components/HelloWorld.vue
@@ -1,58 +1,213 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+  <div id="game">
+
+    <h1>Tic Tac Toe</h1>
+
+    <div v-for="(row, i) in board" :key="i" class="board-row">
+
+      <button v-for="(cell, j) in row" :key="j" @click="makeMove(i, j)">
+
+        {{ cell }}
+
+      </button>
+
+    </div>
+
+    <h2>{{ message }}</h2>
+
+    <button @click="resetBoard">Start Over</button>
+
   </div>
+
 </template>
 
+
+
+
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data() {
+
+    return {
+
+      board: Array(3).fill().map(() => Array(3).fill(null)),
+
+      xIsNext: true,
+
+      message: 'Next player: X'
+
+    }
+
+  },
+
+  methods: {
+
+    checkWinner() {
+
+      const lines = [
+
+        [0, 0, 0, 1, 0, 2],
+
+        [1, 0, 1, 1, 1, 2],
+
+        [2, 0, 2, 1, 2, 2],
+
+        [0, 0, 1, 0, 2, 0],
+
+        [0, 1, 1, 1, 2, 1],
+
+        [0, 2, 1, 2, 2, 2],
+
+        [0, 0, 1, 1, 2, 2],
+
+        [0, 2, 1, 1, 2, 0]
+
+      ];
+
+      for (let line of lines) {
+
+        const [a, b, c, d, e, f] = line;
+
+        if (
+
+          this.board[a][b] &&
+
+          this.board[a][b] === this.board[c][d] &&
+
+          this.board[a][b] === this.board[e][f]
+
+        ) {
+
+          return this.board[a][b];
+
+        }
+
+      }
+
+      return null;
+
+    },
+
+    makeMove(i, j) {
+
+      if (this.board[i][j] || this.checkWinner()) {
+
+        return;
+
+      }
+
+      this.board[i][j] = this.xIsNext ? 'X' : 'O';
+
+      this.xIsNext = !this.xIsNext;
+
+      const winner = this.checkWinner();
+
+      if (winner) {
+
+        this.message = 'Winner: ' + winner;
+
+      } else {
+
+        this.message = 'Next player: ' + (this.xIsNext ? 'X' : 'O');
+
+        if (!this.xIsNext) {
+
+          this.$nextTick(() => {
+
+            this.makeComputerMove();
+
+          });
+
+        }
+
+      }
+
+    },
+
+    makeComputerMove() {
+
+      let available = [];
+
+      for (let i = 0; i < 3; i++) {
+
+        for (let j = 0; j < 3; j++) {
+
+          if (!this.board[i][j]) {
+
+            available.push([i, j]);
+
+          }
+
+        }
+
+      }
+
+      if (available.length) {
+
+        const move = available[Math.floor(Math.random() * available.length)];
+
+        this.makeMove(move[0], move[1]);
+
+      }
+
+    },
+
+    resetBoard() {
+
+      this.board = Array(3).fill().map(() => Array(3).fill(null));
+
+      this.xIsNext = true;
+
+      this.message = 'Next player: X';
+
+    }
+
   }
+
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+#game {
+
+  display: flex;
+
+  flex-direction: column;
+
+  align-items: center;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+
+
+
+.board-row {
+
+  display: flex;
+
+  justify-content: center;
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+
+
+
+button {
+
+  width: 60px;
+
+  height: 60px;
+
+  margin: 4px;
+
 }
-a {
-  color: #42b983;
-}
+
 </style>
